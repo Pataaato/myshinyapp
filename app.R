635412c23ab7dc811aa63d57648d636c24f3feab
Fretault Samantha <samantha.fretault@gmail.com>
2025-02-06 18:08:46 +0000

    Création de l'application newbins

--- app.R
+++ app.R
@@ -2,33 +2,36 @@ library(shiny)
 library(styler)
 
 ui <- fluidPage(
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("My First Shiny"),
+  h2("my app from scratch"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-        "Number of bins:",
+      sliderInput(
+        inputId = "newbins",
+        label = "Number of bins:",
         min = 1,
         max = 50,
         value = 30
       )
     ),
-    mainPanel(plotOutput("distPlot"))
+    mainPanel(
+      plotOutput("distPlot")
+    )
   )
 )
 
 server <- function(input, output) {
   output$distPlot <- renderPlot({
     x <- faithful[, 2]
-    bins <- seq(min(x), 
-                max(x), 
-                length.out = input$bins + 1)
+    newbins <- seq(min(x), max(x), length.out = input$newbins + 1)
+    
     hist(x,
-      breaks = bins, col = "darkgray", 
-      border = "white",
-      xlab = "Waiting time to next eruption (in mins)",
-      main = "Histogram of waiting times"
+         breaks = newbins, col = "darkgray", border = "white",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times"
     )
   })
 }
 
 shinyApp(ui = ui, server = server)
+
