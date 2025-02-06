3b07cdff38be370aae5120adea31d16b8957fc1f
Fretault Samantha <samantha.fretault@gmail.com>
2025-02-06 18:47:01 +0000

    Mise à jour du projet + starwars

--- app.R
+++ app.R
@@ -1,35 +1,36 @@
 library(shiny)
-library(styler)
+library(dplyr)
+library(ggplot2)
 
 ui <- fluidPage(
   titlePanel("My First Shiny"),
+  h1("Star Wars Characters"),
   sidebarLayout(
     sidebarPanel(
-      h2("my app from scratch"),
       sliderInput(
-        inputId = "newbins",
-        label = "Number of bins:",
-        min = 1,
-        max = 50,
-        value = 30
+        inputId = "taille",
+        label = "Height of characters",
+        min = 0,
+        max = 250,
+        value = 50
       )
     ),
     mainPanel(
-      plotOutput("distPlot")
+      plotOutput("StarWarsPlot")
     )
   )
 )
 
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    x <- faithful[, 2]
-    newbins <- seq(min(x), max(x), length.out = input$newbins + 1)
-    
-    hist(x,
-         breaks = newbins, col = "darkgray", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times"
-    )
+  output$StarWarsPlot <- renderPlot({
+    starwars |>
+    filter(height > input$taille) |>
+      ggplot(aes(x = height)) + 
+      geom_histogram(
+        binwidth = 10, 
+        fill = "darkgray", 
+        color = "white"
+      )
   })
 }
 
