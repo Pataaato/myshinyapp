568cbbe232a6f15781d50528ebcb08237802df1c
Fretault Samantha <samantha.fretault@gmail.com>
2025-02-07 18:23:34 +0000

    Choix du thème

--- app.R
+++ app.R
@@ -3,8 +3,14 @@ library(dplyr)
 library(ggplot2)
 library(glue)
 library(DT)
+library(bslib)
+library(thematic)
+
+thematic_shiny(font = "auto")
 
 ui <- fluidPage(
+  theme = bs_theme(version = 5,
+  bootswatch = "sketchy"),
   titlePanel("My First Shiny"),
   h1("Star Wars Characters"),
   selectInput(
@@ -43,8 +49,8 @@ server <- function(input, output) {
       ggplot(aes(x = height)) +
       geom_histogram(
         binwidth = 10,
-        fill = "darkgrey",
-        color = "white"
+        fill = "white",
+        color = "black"
       ) + 
       labs(
         title = glue("Vous avez selectionné le genre : {input$choix_genre}")
